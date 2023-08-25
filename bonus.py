987f8c93b904ca5740bb364e126088fc9a9f9426
NIKHAR BEEJAWAT <nikhar.beejawat@gmail.com>
2023-08-26 00:04:36 +0530

    Changes Updated

--- bonus.py
+++ bonus.py
@@ -1,16 +1,28 @@
 import PySimpleGUI as SG
+from zip_creator import make_archive
 
 label1 = SG.Text("Select Files to compress")
-input_box1 = SG.InputText(tooltip="Select Files")
-add_button1 = SG.FileBrowse("Choose")
+input_box1 = SG.Input()     
+add_button1 = SG.FileBrowse("Choose", key="files")
 
-label2 = SG.Text("Type in a ToDo")
-input_box2 = SG.InputText(tooltip="Compress Loc")
-add_button2 = SG.FolderBrowse("Choose")
+label2 = SG.Text("Select Destination Folder")
+input_box2 = SG.Input()
+add_button2 = SG.FolderBrowse("Choose", key="folder")
 
 compress_button = SG.Button("Compress")
+output_label = SG.Text(key="output")
 
-window = SG.Window("MyApp", layout=[[label1, input_box1, add_button1],[label2, input_box2, add_button2],[compress_button]])
-window.read()
+window = SG.Window("MyApp", layout=[[label1, input_box1, add_button1],[label2, input_box2, add_button2],
+                                    [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression Completed")
+    
+
 window.close()
  
