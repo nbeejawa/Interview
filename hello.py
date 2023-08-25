635cacf2d702a778cc809f9a94c59506ccf14ff7
NIKHAR BEEJAWAT <nikhar.beejawat@gmail.com>
2023-08-25 11:54:03 +0530

    Updated GUI code

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print "hello, world!"
+import PySimpleGUI as SG
+import os
+
+label = SG.Text("Type in a ToDo")
+input_box = SG.InputText(tooltip="Enter ToDo")
+add_button = SG.Button("Add")
+window = SG.Window("MyApp", layout=[[label, input_box, add_button]])
+window.read()
+window.close()
