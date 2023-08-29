31417c1935eeebe82fb14a95734dc1637eb55cfb
NIKHAR BEEJAWAT <nikhar.beejawat@gmail.com>
2023-08-29 12:44:36 +0530

    Contact_Us demo Update

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address")
+    message = st.text_area("Your Message")
+    button = st.form_submit_button("Submit")
+    
+    if button:
+        print("I was pressed")
