95d784bf5da1243073a435129b2e018262e55ee2
NIKHAR BEEJAWAT <nikhar.beejawat@gmail.com>
2023-08-29 12:04:29 +0530

    Streamlit column update

--- bonus5.py
+++ bonus5.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -12,6 +13,25 @@ with col2:
     I am a Python Programmer. Learning To explore more areas
     """
     st.info(content)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
 
 content = """
     I am a Python Programmer. Working on the the content added
