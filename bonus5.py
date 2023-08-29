c8fd5eb81717332bdeb15d21f02638e973d1a801
NIKHAR BEEJAWAT <nikhar.beejawat@gmail.com>
2023-08-29 11:29:52 +0530

    added task and code

---
+++ bonus5.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1: 
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Nikhar Beejawat")
+    content = """
+    I am a Python Programmer. Learning To explore more areas
+    """
+    st.info(content)
+
+content = """
+    I am a Python Programmer. Working on the the content added
+    to show how you are performing the task assigned to modify
+    certain app
+    """
+st.write(content)
